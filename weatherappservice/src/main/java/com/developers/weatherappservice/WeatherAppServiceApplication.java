956987c18de7f0e013d2713b20f3b693db74720d
richardfm77 <ricardo_fm77@ciencias.unam.mx>
2023-07-11 16:31:48 -0600

    init estrcuture proyect server

---
+++ weatherappservice/src/main/java/com/developers/weatherappservice/WeatherAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.developers.weatherappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppServiceApplication.class, args);
+	}
+
+}
